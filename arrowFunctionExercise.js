fda9705ad7978f42944f7aed6fdc8d0cc295a6ba
Tetyana <iltaev2010@gmail.com>
2021-06-16 19:05:10 -0500

    arrow functions exercise finished

---
+++ arrowFunctionExercise.js
@@ -0,0 +1,30 @@
+// function double(arr) {
+//     return arr.map(function(val) {
+//       return val * 2;
+//     });
+//   }
+// Refactor the above code to use two arrow functions. Turn it into a one-liner.
+
+/* Write an ES2015 Version */
+
+
+const double = arr => (arr.map(val => val * 2));
+
+// Refactor the following function to use arrow functions:
+// Replace ALL functions with arrow functions:
+
+// function squareAndFindEvens(numbers){
+//     var squares = numbers.map(function(num){
+//       return num ** 2;
+//     });
+//     var evens = squares.filter(function(square){
+//       return square % 2 === 0;
+//     });
+//     return evens;
+//   }
+
+const squareAndFindEvens = numbers => {
+    const squares = numbers.map(num => num ** 2);
+    return squares.filter(square => (square % 2 === 0));
+    };
+
